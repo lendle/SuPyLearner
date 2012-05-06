f574e470d2fcc9706c6a3fb0b267ad40fcd238d2
Sam Lendle <sam.lendle@gmail.com>
2012-05-06 15:53:12 -0700

    Adding SuPyLearner.py

---
+++ SuPyLearner.py
@@ -0,0 +1,108 @@
+from sklearn import clone, metrics
+from sklearn.base import BaseEstimator, RegressorMixin
+import sklearn.cross_validation as cv
+import numpy as np
+from scipy.optimize import fmin_l_bfgs_b, nnls
+
+class SuperLearner(BaseEstimator, RegressorMixin):
+    def __init__(self, library, K=5, loss='L2', discrete=False, coef_method='L_BFGS_B'):
+        self.library=library[:]
+        self.K=K
+        self.loss=loss
+        self.discrete=discrete
+        self.coef_method=coef_method
+        self.n_estimators=len(library)
+    
+    def fit(self, X, y):
+        n=len(y)
+        
+    
+        folds = cv.KFold(n, 5)
+
+        y_pred_cv = np.empty(shape=(n, self.n_estimators))
+        for train_index, test_index in folds:
+            X_train, X_test=X[train_index], X[test_index]
+            y_train, y_test=y[train_index], y[test_index]
+            for aa in range(self.n_estimators):
+                est=clone(self.library[aa])
+                est.fit(X_train,y_train)
+        
+                y_pred_cv[test_index, aa]=est.predict(X_test)    
+    
+        self.coef=self._get_coefs(self.coef_method, y, y_pred_cv)
+
+        self.fitted_library=clone(self.library)
+        for est in self.fitted_library:
+            est.fit(X, y)
+            
+        self.risk_cv=[]
+        for aa in range(self.n_estimators):
+            self.risk_cv.append(metrics.mean_square_error(y, y_pred_cv[:,aa]))
+        self.risk_cv.append(metrics.mean_square_error(y, np.dot(y_pred_cv, self.coef)))
+                        
+    
+    def predict(self, X):
+        n_X = X.shape[0]
+        y_pred_all = np.empty((n_X,self.n_estimators))
+        for aa in range(self.n_estimators):
+            y_pred_all[:,aa]=self.fitted_library[aa].predict(X)
+        y_pred=np.dot(y_pred_all, self.coef)
+        return y_pred
+
+
+    def summarize(self):
+        print "Cross-validated risk estimates for each estimator in the library:"
+        print self.risk_cv[:-1]
+        print "Coefficients:", self.coef
+        print "Estimated risk for SL:", self.risk_cv[-1]
+    
+    def _get_coefs(self, method, y, y_pred_cv):
+        if method is 'L_BFGS_B':
+            def ff(x):
+                return sum((y-np.dot(y_pred_cv, x))**2)
+            x0=np.array([1/self.n_estimators]*self.n_estimators)
+            bds=[(0,1)]*self.n_estimators
+            a,b,c=fmin_l_bfgs_b(ff, x0, bounds=bds, approx_grad=True)
+            coef=a/sum(a)
+        elif method is 'NNLS':
+            init_coef, rnorm=nnls(y_pred_cv, y)
+            coef=init_coef/sum(init_coef)        
+        else: raise ValueError("method not recognized")
+        return coef   
+    
+    
+# def clone(estimator, safe=True):
+#     try:
+#         r=sklearn.clone(estimator, safe)
+#     except AssertionError as e:
+#         if e[0][0].split()[-1] is 'library':
+#             r=sklearn.clone(estimator, False)
+#         else:
+#             raise AssertionError(e)
+#     return r
+        
+        
+    
+
+def CVSuperLearner(library, X, y, K):
+    sl=SuperLearner(library)
+    library=library+[sl]
+    
+    n=len(y)
+    folds=cv.KFold(n, K)
+    y_pred_cv = np.empty(shape=(n, len(library)))
+
+    for train_index, test_index in folds:
+        X_train, X_test=X[train_index], X[test_index]
+        y_train, y_test=y[train_index], y[test_index] 
+        for aa in range(len(library)):
+            est=library[aa]
+            est.fit(X_train,y_train)
+            y_pred_cv[test_index, aa]=est.predict(X_test)
+
+    risk_cv=[]        
+    for aa in range(len(library)):
+        risk_cv.append(metrics.mean_square_error(y, y_pred_cv[:,aa]))
+    
+    print risk_cv
+    
